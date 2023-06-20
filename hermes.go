24d031ded52e2f2b4671310460197f092ccca840
Steve James <0x2t1ff@gmail.com>
2023-06-20 11:43:39 +0200

    debugging cloudwatch

--- hermes.go
+++ hermes.go
@@ -1,9 +1,10 @@
 package hermes
 
 import (
-  "fmt"
-  "os"
-  "time"
+	"fmt"
+	"os"
+	"time"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -11,55 +12,58 @@ import (
 	"github.com/rs/zerolog"
 )
 
-
 var logger zerolog.Logger
 
 type CloudWatchWriter struct {
-  logGroupName string
-  logStreamName string
-  service *cloudwatchlogs.CloudWatchLogs
+	logGroupName  string
+	logStreamName string
+	service       *cloudwatchlogs.CloudWatchLogs
 }
 
 // Generate the current timestamp as an int64
 func makeTimestamp() int64 {
-    return time.Now().UnixNano() / int64(time.Millisecond)
+	return time.Now().UnixNano() / int64(time.Millisecond)
 }
 
 func (cw *CloudWatchWriter) Write(p []byte) (n int, err error) {
-  input := &cloudwatchlogs.PutLogEventsInput{
-    LogGroupName: aws.String(cw.logGroupName),
-    LogStreamName: aws.String(cw.logStreamName),
-    LogEvents: []*cloudwatchlogs.InputLogEvent{
-      {
-        Message: aws.String(string(p)),
-        Timestamp: aws.Int64(makeTimestamp()),
-      },
-    },
-  }
+	input := &cloudwatchlogs.PutLogEventsInput{
+		LogGroupName:  aws.String(cw.logGroupName),
+		LogStreamName: aws.String(cw.logStreamName),
+		LogEvents: []*cloudwatchlogs.InputLogEvent{
+			{
+				Message:   aws.String(string(p)),
+				Timestamp: aws.Int64(makeTimestamp()),
+			},
+		},
+	}
 
-  _, err = cw.service.PutLogEvents(input)
-  if err != nil {
+	_, err = cw.service.PutLogEvents(input)
+	if err != nil {
 		if awsErr, ok := err.(awserr.Error); ok {
 			if awsErr.Code() == cloudwatchlogs.ErrCodeResourceNotFoundException {
-          fmt.Println("A CloudWatch LogGroup does not exist with this name. Verify that it exists or that you are not initialising the Logger with the wrong name")
-					panic(err.Error())
+				fmt.Println("A CloudWatch LogGroup does not exist with this name. Verify that it exists or that you are not initialising the Logger with the wrong name")
+				panic(err.Error())
+			} else {
+				panic(err.Error())
 			}
 		} else {
 			fmt.Println("Failed to cast error to awserr.Error:", err)
 		}
-    return 0, err
-  }
+		fmt.Println(err.Error())
+		return 0, err
+	}
 
-  return len(p), nil
+	return len(p), nil
 }
 
 // Initialise the Hermes and its logging components.
 // svcName is used as the logGroupName.
 func Init(svcName string) {
-  env := os.Getenv("ENV")
 
-  zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	fmt.Println("Init logging")
+	env := os.Getenv("ENV")
 
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
 
 	if env == "dev" {
 		logger = zerolog.New(os.Stdout).With().Timestamp().Logger()
@@ -71,30 +75,31 @@ func Init(svcName string) {
 			panic(err)
 		}
 
-    logGroup := fmt.Sprintf("%s-%s", svcName, env)
+		logGroup := fmt.Sprintf("%s-%s", svcName, env)
 		podName := os.Getenv("HOSTNAME")
-    service := cloudwatchlogs.New(sess)
-    
-    // Create the Log Stream if it doesn't exist
-    _, err = service.CreateLogStream(&cloudwatchlogs.CreateLogStreamInput{
-      LogGroupName:  aws.String(logGroup),
-      LogStreamName: aws.String(podName),
-    })
-    if err != nil {
-      if awsErr, ok := err.(awserr.Error); ok {
-        // Ignore error if the Log Stream already exists
-        if awsErr.Code() != cloudwatchlogs.ErrCodeResourceAlreadyExistsException {
-          panic(err)
-        }
-      } else {
-        panic(err)
-      }
-    } 
+		service := cloudwatchlogs.New(sess)
+
+		// Create the Log Stream if it doesn't exist
+		_, err = service.CreateLogStream(&cloudwatchlogs.CreateLogStreamInput{
+			LogGroupName:  aws.String(logGroup),
+			LogStreamName: aws.String(podName),
+		})
+		if err != nil {
+			if awsErr, ok := err.(awserr.Error); ok {
+				// Ignore error if the Log Stream already exists
+				if awsErr.Code() != cloudwatchlogs.ErrCodeResourceAlreadyExistsException {
+					panic(err)
+				}
+			} else {
+				panic(err)
+			}
+			panic(err)
+		}
 
 		writer := &CloudWatchWriter{
-			logGroupName: logGroup,
+			logGroupName:  logGroup,
 			logStreamName: podName,
-			service: service,
+			service:       service,
 		}
 
 		logger = zerolog.New(writer).With().Timestamp().Logger()
@@ -102,7 +107,6 @@ func Init(svcName string) {
 }
 
 // Public function to retrieve the initialised logger
-func Logger() zerolog.Logger{
-  return logger
+func Logger() zerolog.Logger {
+	return logger
 }
-
